506348cea63c050634cb925b4a9ebf6d8936b15e
wataru.otsuji <80444322+wotsuji@users.noreply.github.com>
2021-04-09 13:02:02 +0900

    Add files via upload

---
+++ website_monitoring_status_code.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Website monitoring.
+echo ----
+
+@set check_urls[0]=https://googl.co.jp
+@set check_urls[1]=https://example2.co.jp
+@set check_urls[2]=https://example3.co.jp
+@set check_urls[3]=https://example4.co.jp
+
+set k=0
+:LOOP
+  call set check_url=%%check_urls[%k%]%%
+  if defined check_url (
+    set http_code=
+    FOR /F %%i in ('curl.exe -s -k -I -L -o nul -w "%%{http_code}" %check_url%') DO @SET http_code=%%i
+    
+    echo %check_url%
+    if !http_code! neq 200 (
+      echo -^> !http_code! NG
+    ) else (
+      echo -^> !http_code! OK
+    )
+    set /a k=%k%+1
+    goto LOOP
+  )
+:LOOP_END
+
+echo ----
+
+pause
+
