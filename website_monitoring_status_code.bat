a1c75b3de692cf1a4fa0de7e8a883d5fbc79c3c5
wataru.otsuji <wataru.otsuji@gmail.com>
2021-04-09 13:38:08 +0900

    行頭に @echo off があるため途中の@を削除

--- website_monitoring_status_code.bat
+++ website_monitoring_status_code.bat
@@ -4,17 +4,17 @@ setlocal enabledelayedexpansion
 echo Website monitoring.
 echo ----
 
-@set check_urls[0]=https://example1.co.jp
-@set check_urls[1]=https://example2.co.jp
-@set check_urls[2]=https://example3.co.jp
-@set check_urls[3]=https://example4.co.jp
+set check_urls[0]=https://example1.co.jp
+set check_urls[1]=https://example2.co.jp
+set check_urls[2]=https://example3.co.jp
+set check_urls[3]=https://example4.co.jp
 
 set k=0
 :LOOP
   call set check_url=%%check_urls[%k%]%%
   if defined check_url (
     set http_code=
-    FOR /F %%i in ('curl.exe -s -k -I -L -o nul -w "%%{http_code}" %check_url%') DO @SET http_code=%%i
+    FOR /F %%i in ('curl.exe -s -k -I -L -o nul -w "%%{http_code}" %check_url%') DO SET http_code=%%i
     
     echo %check_url%
     if !http_code! neq 200 (
